67a1112549ec441bcc9c336d2e4ff28133891bc0
Aleksandr Grigorii <domosedov.dev@gmail.com>
2022-12-27 10:14:45 +0300

    bump deps

--- model.ts
+++ model.ts
@@ -1,6 +1,6 @@
 import { createQuery } from "@farfetched/core";
-import { createEffect, createStore } from "effector";
-import { status } from "patronum";
+import { createEffect, createEvent, createStore, restore } from "effector";
+import { snapshot, status } from "patronum";
 import { createCounter } from "./factory";
 
 export const $count1 = createStore(0);
@@ -9,6 +9,11 @@ export const $count2 = createStore(0);
 // Ok
 console.log("[Stores sids]:", $count1.sid, $count2.sid);
 
+const $sn1 = snapshot({ source: $count1 });
+const $sn2 = snapshot({ source: $count1 });
+
+console.log("[Snapshot sids]:", $sn1.sid, $sn2.sid);
+
 const fx1 = createEffect(() => 42);
 const fx2 = createEffect(() => 42);
 
@@ -16,7 +21,9 @@ const fx2 = createEffect(() => 42);
 console.log("[Effect sids]:", fx1.sid, fx2.sid);
 
 export const $s1 = status({ effect: fx1 });
+
 export const $s2 = status({ effect: fx1 });
+
 export const $s3 = status({ effect: fx1 });
 
 // Bug
